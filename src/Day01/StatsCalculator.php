045974a0decd7c6ff72282a994e1c00503af23fa
Steven Ellis <steve.ellis@mademedia.co.uk>
2013-05-07 21:32:36 +0100

    Added folder structure

---
+++ src/Day01/StatsCalculator.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Day01;
+
+class StatsCalculator {
+
+	public function run(array $numbers)
+	{
+		
+	}
+
+}
---
+++ tests/StatsCalculatorTest.php
@@ -0,0 +1,5 @@
+<?php 
+
+class StatsCalculatorTest extends \PHPUnit_Framework_TestCase {
+	
+}
